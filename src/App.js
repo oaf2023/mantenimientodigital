9a803ea4cb2a2e6f0c9fa0faddb64b52e5bbac64
oaf2023 <148269180+oaf2023@users.noreply.github.com>
2025-02-05 20:15:43 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
