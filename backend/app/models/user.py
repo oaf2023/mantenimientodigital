9c6adcafa328b0e38d65101179be11bb145e1602
oaf2023 <148269180+oaf2023@users.noreply.github.com>
2025-02-05 20:04:34 -0300

    Update user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,1 +1,11 @@
-print("User")
+# backend/config.py
+import os
+from pymongo import MongoClient
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'tu_clave_secreta')
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
+    DATABASE_NAME = 'mantenimientodigital'
+
+client = MongoClient(Config.MONGO_URI)
+db = client[Config.DATABASE_NAME]
